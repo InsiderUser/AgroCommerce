ad77b9ceabcf33d2b601069fddc370756a949cf0
InsiderUser <gonzaruizok@gmail.com>
2023-08-22 11:03:08 -0300

    Acceso por email/usuario agregado

--- template/backend/login.php
+++ template/backend/login.php
@@ -1,22 +1,31 @@
    <?php
     include "conection.php";
-//    $flag = false;
+   $flag = false;
     //Obtener user/pass del form y parsing a minuscula
     $username = $_POST['webUser'];
     $password = $_POST['webPass'];
 
+   if(strpos($username,'@') == true){
+    $input = pg_query($conectado,"SELECT correo,clave FROM clientes WHERE correo='$username'");
+    $flag = true;
+   }else{
+    $input = pg_query($conectado,"SELECT usuario,clave FROM clientes WHERE usuario='$username'"); 
+    $flag = false; 
+   }
     
-    $input = pg_query($conectado,"SELECT usuario,clave FROM clientes WHERE usuario='$username'");
     $input = pg_fetch_assoc($input);
     
-    if($input && $input['usuario']==$username && $input['clave']==$password){
+    if($input && $input['usuario']==$username && $input['clave']==$password && $flag == false){
+        header('Location: http://localhost/agrocommerce/template/index.html');
+        
+        exit();
+    }elseif($input && $input['correo']==$username && $input['clave']==$password && $flag == true){
         header('Location: http://localhost/agrocommerce/template/index.html');
         exit();
     }else{
         $error = true;
         header('Location: http://localhost/agrocommerce/template/pages/samples/login.html');
         exit();
-        ## BUG
-        ## Deberia mostrar un modal durante unos segundos indicando que hay errores en las credenciales de acceso
+      
     }
     ?>
