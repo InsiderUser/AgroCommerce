4d58e1c6306446be705261b0509f13975de754b0
Alejandro <romanazzialejandro2014@gmail.com>
2023-10-03 00:17:27 -0300

    perceptron integrado a AgroCommerce, se ejecuta a traves de consola invocando el archivo perceptron_agro.py, y el valor resultante se muestra en layout (arriba del navbar).

--- src/app/Perceptron/perceptron_agro.py
+++ src/app/Perceptron/perceptron_agro.py
@@ -1,6 +1,35 @@
 from main import Perceptron
 import numpy as np
 import csv
+import psycopg2
+
+# Configura los detalles de la conexión
+host = "localhost"
+port = "5432"
+database = "agrocommercedb"
+user = "postgres"
+password = "postgres"
+
+# Crea la conexión
+conn = psycopg2.connect(
+    host=host,
+    port=port,
+    database=database,
+    user=user,
+    password=password
+)
+
+cur = conn.cursor()
+
+cur.execute("SELECT * FROM perceptron WHERE id = 1")
+
+# Recupera los resultados de la consulta
+resultados = cur.fetchall()
+
+# Imprime los resultados
+for fila in resultados:
+    print(fila)
+
 
 valores = []
 etiquetas = []
@@ -17,8 +46,8 @@ lista_data_values_int = [
     [float(valor) for valor in sublista] for sublista in data_values
 ]
 lista_data_values_int.pop()
-
-valores = np.array(lista_data_values_int)
+print(lista_data_values_int)
+#valores = np.array(lista_data_values_int)
 
 # Define y crea el perceptrón con el número correcto de entradas
 perceptron_and = Perceptron(2)
@@ -40,9 +69,29 @@ valores = np.append(valores, [ph, humedad, resultado])
 print("Resultado:", resultado)
 
 # Abrir el archivo CSV en modo de escritura
-with open("C:/xampp/htdocs/AgroCommerce/src/app/Perceptron/datasets/" + cultivo + "_dataset.csv", mode="a", newline="") as File:
-    escritor_csv = csv.writer(File)
-    # Escribe el nuevo elemento en una fila separada
-    escritor_csv.writerow([ph, humedad, resultado])
+#with open("C:/xampp/htdocs/AgroCommerce/src/app/Perceptron/datasets/" + cultivo + "_dataset.csv", mode="a", newline="") as File:
+#    escritor_csv = csv.writer(File)
+#    # Escribe el nuevo elemento en una fila separada
+#    escritor_csv.writerow([ph, humedad, resultado])
 
-print("Nuevo valor agregado al archivo CSV.")
+#print("Nuevo valor agregado al archivo CSV.")
+res = int(resultado)
+sql = "UPDATE perceptron SET valores = %s WHERE id = 1"
+cur.execute(sql, (res,))
+conn.commit()
+
+
+# Realiza una consulta SELECT para mostrar los cambios
+sql_select = "SELECT * FROM perceptron WHERE id = 1"
+
+# Ejecuta la consulta SELECT
+cur.execute(sql_select)
+
+# Recupera los resultados de la consulta
+resultado = cur.fetchone()
+
+# Imprime los resultados
+print(resultado)
+
+cur.close()
+conn.close()
