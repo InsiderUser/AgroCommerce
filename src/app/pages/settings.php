2d1c5ff1c2a21bd852c19cab0ba385a321d79748
InsiderUser <gonzaruizok@gmail.com>
2023-10-21 13:17:41 -0300

    Modificación de los cultivos finalizada | Modificacion sobre el boton eliminar, el cual paso a ser parte de un 'form' para poder realizar la eliminacion del cultivo

--- src/app/pages/settings.php
+++ src/app/pages/settings.php
@@ -296,13 +296,15 @@ function getCrops($conectado, $userId,$flag){
                                                         $flag=1;
                                                         getCrops($conectado,$userId,$flag);
                                                     ?></h5>
-                                        <button class="btn btn-danger" type="button" id="deleteCrop">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"></path>
-                                                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"></path>
-                                            </svg> 
-                                            Eliminar
-                                        </button>
+                                        <form action="../../backend/deleteCrops.php" method="post">
+                                            <button class="btn btn-danger" type="submit" id="deleteCrop" name="deleteCrop1" >
+                                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"></path>
+                                                 <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"></path>
+                                             </svg> 
+                                             Eliminar
+                                            </button>
+                                        </form>
                                     </div>
                                     <form class="needs-validation" name="" method="post" action="../../backend/updateCropsData.php" novalidate>
                                         <div class="col-lg-6 col-md-8 col-sm-12">
@@ -471,15 +473,21 @@ function getCrops($conectado, $userId,$flag){
                                                         $flag=4;
                                                         getCrops($conectado,$userId,$flag);
                                                     ?></h5>
-                                        <button class="btn btn-danger" type="button" id="deleteCrop">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"></path>
-                                                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"></path>
-                                            </svg> 
-                                            Eliminar
-                                        </button>
+
+                                                    
+                                        <form action="../../backend/deleteCrops.php" method="post">
+                                            <button class="btn btn-danger" type="submit" id="deleteCrop" name="deleteCrop2" >
+                                              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"></path>
+                                                 <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"></path>
+                                             </svg> 
+                                             Eliminar
+                                            </button>
+                                        </form>
+
+                                       
                                     </div>
-                                    <form class="needs-validation" name="<?php ?>" method="post" action="../../backend/updateCropsData.php" novalidate>
+                                    <form class="needs-validation" name="" method="post" action="../../backend/updateCropsData.php" novalidate>
                                         <div class="col-lg-6 col-md-8 col-sm-12">
                                             <!-- Provincia -->
                                             <div class="form-group mt-2">
