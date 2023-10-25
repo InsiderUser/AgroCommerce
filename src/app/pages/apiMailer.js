dde824e0ac0f2612a17392953ef5a2393b6aeb06
InsiderUser <gonzaruizok@gmail.com>
2023-10-25 02:04:02 -0300

    Ajustes en el mensaje de envio | Envio de mensajes dinamico segun sesion iniciada

--- src/app/pages/apiMailer.js
+++ src/app/pages/apiMailer.js
@@ -15,10 +15,9 @@ enviarMail = async (correo) => {
   const mensaje = {
     from: '"AgroCommerce" <agrocommercele@gmail.com>', // sender address
     to: correo, // list of receivers
-    // to: `gonzaruizok@gmail.com`,
-    subject: "Notificacion de riego", // Subject line
+    subject: "NOTIFICACION DE RIEGO", // Subject line
     text: "Esto es una notifiacion automatizada", // plain text body
-    html: "Se le notifica que <b>en breve comenzará</b>, segun el intervalo que ha establecido en nuestra plataforma.", // html body
+    html: 'Estimado usuario:<br><b>El riego de tus cultivos comenzará en breve</b> de acuerdo al intervalo establecido en la configuracion, ¡prepárate para el éxito de tus cultivos!<br><br>•Si deseas modificar el intervalo, lo podes hacer clickeando sobre <a href="http://localhost/agrocommerce/src/app/pages/settings.php">este vínculo</a><br>•Si tenes alguna pregunta o precisas asistencia adicional, en nuestra sección de <a href="http://localhost/agrocommerce/src/app/pages/support.php">soporte</a> podrás revisar las preguntas frecuentes o contactarte directamente con nosotros.<br><br>Saludos,<br>El equipo de Agrocommerce<br><i>Este es un mensaje automático. Por favor, no responda a este correo</i><br><img src="https://drive.google.com/file/d/1Jj0S0RMtsUq81l29qj9BcRKY_1C1ZuBO/view?usp=sharing">.', // html body
   };
 
   const transporter = nodeMailer.createTransport(config);
